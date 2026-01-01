f160f147ab5e9cd6faf557ffde2c42cc6d6d3a40
Reinascape <reinascape@gmail.com>
2026-01-02 01:12:51 +0300

    sm8650-common: Switch to oplus sensors AIDL

--- common.mk
+++ common.mk
@@ -349,7 +349,7 @@ endif
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.multihal \
+    android.hardware.sensors-service.oplus-multihal \
     sensors.dynamic_sensor_hal \
     sensors.oplus
 
